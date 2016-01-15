5e2c29452b8baf13044de8521bfaa720b14be437
XuJian <xujian@XuJians-MacBook-Pro.local>
2016-01-15 13:32:31 -0800

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by XuJian on 1/15/16.
+//  Copyright (c) 2016 Jian (Kevin) Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homepwner
+//
+//  Created by XuJian on 1/15/16.
+//  Copyright (c) 2016 Jian (Kevin) Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
